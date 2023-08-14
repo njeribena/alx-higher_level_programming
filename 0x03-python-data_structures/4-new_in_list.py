d2b861172a89632e1ff301bf1d5faf6b9b28a99f
njeribena <njeriwaweru94@gmail.com>
2023-08-14 11:20:57 -0700

    4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+
+    if my_list is None:
+        return
+    if idx < 0:
+        return new_list
+    if idx > (len(my_list) - 1):
+        return new_list
+
+    new_list[idx] = element
+    return new_list
